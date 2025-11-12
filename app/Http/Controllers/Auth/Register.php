e384ebe24433e81163771e713168eda322b9c390
NepNepTea <162781744+NepNepTea@users.noreply.github.com>
2025-11-12 10:56:01 +0700

    not important

--- app/Http/Controllers/Auth/Register.php
+++ app/Http/Controllers/Auth/Register.php
@@ -1,67 +1,27 @@
 <?php
-
-
-
 namespace App\Http\Controllers\Auth;
 
-
-
 use App\Http\Controllers\Controller;
-
 use App\Models\User;
-
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\Hash;
 
-
-
 class Register extends Controller
-
 {
-
     public function __invoke(Request $request)
-
     {
-
-        // Validate the input
-
         $validated = $request->validate([
-
             'name' => 'required|string|max:30|regex:/^[а-яА-ЯЁё\s]+$/',
-
             'email' => 'required|string|email|max:255|unique:users',
-
             'password' => 'required|string|confirmed|min:8|regex:/^[a-zA-Z0-9]+$/',
-
         ]);
-
-
-
-        // Create the user
-
         $user = User::create([
-
             'name' => $validated['name'],
-
             'email' => $validated['email'],
-
             'password' => Hash::make($validated['password']),
-
         ]);
-
-        // Log them in
-
         Auth::login($user);
-
-
-
-        // Redirect to home
-
         return redirect('/')->with('success', 'Добро пожаловать в River Planet');
-
     }
-
 }
