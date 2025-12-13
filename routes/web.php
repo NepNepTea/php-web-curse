9b38392f989b0927539774589a2ac776c86b48ab
NepNepTea <162781744+NepNepTea@users.noreply.github.com>
2025-12-13 17:51:44 +0700

    small fixes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Login;
@@ -12,138 +11,93 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\OrderController;
 use App\Models\Brand;
 use App\Http\Middleware\IsAdmin;
-
 Route::get('/', function () {
     return view('index');
 })->name('index');
-
 Route::get('/sales', function () {
     return view('sales');
 })->name('sales');
-
 Route::get('/guide', function () {
     return view('guide');
 })->name('guide');
-
-Route::get('/cancel-order', function () {
-    return view('cancel-order');
-})->name('cancel-order');
-
-Route::get('/delete-profile', function () {
-    return view('delete-profile');
-})->name('delete-profile');
-
 Route::view('/register', 'auth.register')
-
     ->middleware('guest')
-
     ->name('register');
-
 Route::post('/register', Register::class)
-
     ->middleware('guest')
-
     ->name('register');
-
 Route::view('/login', 'auth.login')
     ->middleware('guest')
     ->name('login');
-
 Route::post('/login', Login::class)
     ->middleware('guest')
     ->name('login');
-
 Route::view('/logout-page', 'logout')
     ->name('logout-page');
-
 Route::post('/logout', Logout::class)
     ->middleware('auth')
     ->name('logout');
-
 Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog');
-
-
 Route::view('/catalog-add', 'catalog-add', ['brands' => Brand::all()])
-
     ->name('catalog-add')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::post('/catalog-add', [CatalogController::class, 'create'])->name('catalog-add')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::get('/catalog-admin', [CatalogController::class, 'admin'])
     ->name('catalog-admin')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::delete('/catalog-admin/{id}', [CatalogController::class, 'destroy'])
     ->name('delete-product')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::post('/catalog-admin/{id}', [CatalogController::class, 'update'])
     ->name('update-product')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::view('/brand-add', 'brand-add')
-
     ->name('brand-add')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::post('/brand-add', [BrandController::class, 'create'])->name('brand-add')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::get('/brand-admin', [BrandController::class, 'admin'])
     ->name('brand-admin')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::delete('/brand-admin/{id}', [BrandController::class, 'destroy'])
     ->name('delete-brand')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::get('/constructor', [ConstructorController::class, 'index'])->name('constructor')
     ->middleware('auth');
-
 Route::post('/constructor/{id}', [ConstructorController::class, 'add'])
     ->name('constructor-add')
     ->middleware('auth');
-
 Route::post('/cart/{id}', [CartController::class, 'add'])
     ->name('cart-add')
     ->middleware('auth');
-
 Route::get('/cart', [CartController::class, 'index'])->name('cart')
     ->middleware('auth');
-
 Route::get('/cart/{id}', [CartController::class, 'delete'])->name('cart-delete')
     ->middleware('auth');
-
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile')
     ->middleware('auth');
-
 Route::get('/orders-admin', [OrderController::class, 'admin'])
     ->name('orders-admin')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::post('/orders-admin/{id}', [OrderController::class, 'update'])
     ->name('update-order')
     ->middleware(IsAdmin::class)
     ->middleware('auth');
-
 Route::get('/checkcon', [OrderController::class, 'buycon'])->name('checkFromConstructor')
     ->middleware('auth');
-
 Route::get('/checkcar', [OrderController::class, 'buycar'])->name('checkFromCart')
     ->middleware('auth');
-
 Route::post('/order-add/{page}', [OrderController::class, 'check'])
     ->name('order-add')
     ->middleware('auth');
